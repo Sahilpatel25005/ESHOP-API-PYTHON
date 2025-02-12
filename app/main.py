eb1c9910dcea75850333da9a3451a537c459624c
sahil asodariya <sahilpatel252005@gmail.com>
2025-02-12 10:21:55 +0530

    add cart api

--- app/main.py
+++ app/main.py
@@ -1,12 +1,43 @@
-from fastapi import FastAPI
-from app.routes import login, product, register
+from fastapi import FastAPI , Request
+import logging
+from fastapi.middleware.cors import CORSMiddleware
+from app.routes import login, product, register , cart
 import uvicorn
+
+from app import verify_token
 
 app = FastAPI(title="eShop API")
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.StreamHandler()
+    ],
+)
+
+# CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins. Change to specific domains in production!
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    logging.info(f"Incoming {request.method} request: {request.url}")
+    if request.method in ['POST', 'PUT', 'DELETE']:
+        body = await request.body()
+        logging.info(f"Request body: {body.decode('utf-8')}")
+    response = await call_next(request)
+    logging.info(f"Response status code: {response.status_code}")
+    return response 
+
 # Include Routers
 app.include_router(login.login_router)
-app.include_router(login.protected_router)
+app.include_router(cart.add_cart_router)
 app.include_router(product.product)
 app.include_router(register.router)
 
