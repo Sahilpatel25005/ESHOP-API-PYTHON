4bdaec0a0c668c31c4f41a1bdc1e0deb42e484fd
sahil asodariya <sahilpatel252005@gmail.com>
2025-03-01 16:59:25 +0530

    remove file handler from logger configuration to simplify logging setup

--- app/Logger_config.py
+++ app/Logger_config.py
@@ -1,14 +1,14 @@
 import logging
 
 
-file_handler = logging.FileHandler('app.log', mode='a')
+# file_handler = logging.FileHandler('app.log', mode='a')
 stream_handler = logging.StreamHandler()
 
 # Configure logging
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
-    handlers=[file_handler, stream_handler]  # Append to the log file ],
+    handlers=[stream_handler]  # Append to the log file ],
 )
 
 logger = logging.getLogger('my_project_logger')
