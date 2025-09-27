98e45fccc0975b8824cc4e02b2a0261d1df2d481
sahil asodariya <sahilpatel252005@gmail.com>
2025-09-27 16:38:16 +0530

    feat: add generate product details endpoint with AI integration and category retrieval

---
+++ app/routes/generate_product_details.py
@@ -0,0 +1,74 @@
+import json
+import re
+import os
+from dotenv import load_dotenv
+from fastapi import UploadFile, File, APIRouter, HTTPException
+import google.generativeai as genai
+from google.api_core.exceptions import GoogleAPICallError
+from app.database import get_db_connection
+
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("YOUR_API_KEY"))
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+generate_product_details_router = APIRouter(prefix="", tags=["/generate-product"])
+
+@generate_product_details_router.post("/generate-product")
+async def generate_product(image: UploadFile = File(...)):
+    try:
+        img_bytes = await image.read()
+        
+        # get product category from databse
+        conn = get_db_connection()
+        cur = conn.cursor()
+        query = "select name from category"
+        cur.execute(query)
+        categories = cur.fetchall()
+        if not categories:
+            raise HTTPException(status_code=500, detail="No categories found in database")
+          
+        print(categories)
+        
+
+        response = model.generate_content([
+            """You are an assistant that generates e-commerce product data.
+            Task:
+            - Look at the given image.
+            - Generate a short, catchy product title.
+            - Generate a clear, engaging product description (2–3 sentences).
+            - Return output strictly in JSON format only.
+            Example:
+            {
+              "title": "Trendy Black Aviator Sunglasses",
+              "description": "Sleek and stylish aviators with UV protection, lightweight frame, and a modern edge."
+            }
+            """,
+            {"mime_type": image.content_type, "data": img_bytes}
+        ])
+
+        raw_text = response.text.strip()
+
+        # 🔹 Extract JSON using regex (in case Gemini adds extra text)
+        match = re.search(r"\{.*\}", raw_text, re.DOTALL)
+        if not match:
+            raise HTTPException(status_code=500, detail="No valid JSON found in Gemini response")
+
+        json_text = match.group(0)
+
+        try:
+            product_data = json.loads(json_text)
+        except json.JSONDecodeError:
+            raise HTTPException(status_code=500, detail="Gemini returned malformed JSON")
+
+        return product_data
+
+    except GoogleAPICallError as e:
+        raise HTTPException(status_code=500, detail=f"Gemini API error: {e.message}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
+    finally:
+      cur.close()
+      conn.close()
+    
