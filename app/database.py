79360bf57e3bf22b559727b1aebb82014fccb17f
sahil asodariya <sahilpatel252005@gmail.com>
2025-11-20 20:04:23 +0530

    change the databse collections

--- app/database.py
+++ app/database.py
@@ -1,18 +1,20 @@
 import psycopg2
 import psycopg2.extras
-import json
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-with open("app/DB.json", "r") as json_file:
-    DB_CONFIG = json.load(json_file)
+# with open("app/DB.json", "r") as json_file:
+#     DB_CONFIG = json.load(json_file)
 
 # Utility function to connect to the database
 def get_db_connection():
     return psycopg2.connect(
-        host=DB_CONFIG["host"],
-        dbname=DB_CONFIG["dbname"],
-        user=DB_CONFIG["user"],
-        password=DB_CONFIG["password"],
-        port=DB_CONFIG["port"],
+        host=os.getenv("HOST"),
+        dbname=os.getenv("DBNAME"),
+        user=os.getenv("USER"),
+        password=os.getenv("PASSWORD"),
+        port=5432,
         cursor_factory=psycopg2.extras.DictCursor,
     )
     
